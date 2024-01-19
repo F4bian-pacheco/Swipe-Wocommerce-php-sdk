cfd1e8cf4197906c345eace7d8020a2d40876195
f4bian-pacheco <fabian.paxe@gmail.com>
2024-01-19 11:18:00 -0300

    Add Operacion class to handle account operations

---
+++ classes/Operacion.php
@@ -0,0 +1,10 @@
+<?php  
+namespace Swipe\lib;
+
+class Operacion {
+    public $account_id; //string
+    public $amount; //float
+    public $currency; //string
+    public $refence; //string
+    public $signature; //string
+}
